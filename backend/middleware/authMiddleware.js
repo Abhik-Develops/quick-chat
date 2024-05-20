a6f7546bdb2d8ffa4812d1d3d1ccf1243f18866b
Abhik Ganguly <134844120+Abhik-Develops@users.noreply.github.com>
2024-05-20 19:21:28 +0530

    bugs fixed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ const protect = asyncHandler(async (req, res, next) => {
         try {
             token = req.headers.authorization.split(" ")[1];
             const decoded = verifyToken(token);
-            req.user = await UserModel.findById(decoded._id).select("-password");
+            req.user = await UserModel.findById(decoded.id).select("-password");
             next()
         } catch (error) {
             res.status(401);
